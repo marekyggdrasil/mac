b3349358a459275df8d345a1010689bd7c879a45
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-11-13 20:16:28 +0100

    Progress on serialization and deserialization

--- contracts/src/preimage.test.ts
+++ contracts/src/preimage.test.ts
@@ -11,29 +11,31 @@ import {
 
 import { Participant, Outcome, Preimage } from './preimage';
 
-describe('hmmm', () => {
-  beforeAll(async () => {
+describe('Preimage tests', () => {
+  let employer: Participant,
+    contractor: Participant,
+    arbiter: Participant,
+    outcome_deposited: Outcome,
+    outcome_success: Outcome,
+    outcome_failure: Outcome,
+    outcome_cancel: Outcome,
+    mac_contract: Preimage;
+
+  beforeEach(async () => {
     await isReady;
-  });
-
-  afterAll(async () => {
-    setTimeout(shutdown, 0);
-  });
-
-  it('some bs test to get started', () => {
     const employer_sk: PrivateKey = PrivateKey.random();
     const employer_pk: PublicKey = employer_sk.toPublicKey();
-    const employer: Participant = new Participant(employer_pk);
+    employer = new Participant(employer_pk);
 
     const contractor_sk: PrivateKey = PrivateKey.random();
     const contractor_pk: PublicKey = contractor_sk.toPublicKey();
-    const contractor: Participant = new Participant(contractor_pk);
+    contractor = new Participant(contractor_pk);
 
     const arbiter_sk: PrivateKey = PrivateKey.random();
     const arbiter_pk: PublicKey = arbiter_sk.toPublicKey();
-    const arbiter: Participant = new Participant(arbiter_pk);
+    arbiter = new Participant(arbiter_pk);
 
-    const outcome_deposited: Outcome = new Outcome(
+    outcome_deposited = new Outcome(
       CircuitString.fromString(''),
       new Field(12),
       new Field(6),
@@ -42,7 +44,7 @@ describe('hmmm', () => {
       new Field(-1)
     );
 
-    const outcome_success: Outcome = new Outcome(
+    outcome_success = new Outcome(
       CircuitString.fromString('The contractor successfully did the job'),
       new Field(6),
       new Field(12),
@@ -51,7 +53,7 @@ describe('hmmm', () => {
       new Field(-1)
     );
 
-    const outcome_failure: Outcome = new Outcome(
+    outcome_failure = new Outcome(
       CircuitString.fromString('The contractor failed to do the job on time'),
       new Field(11),
       new Field(5),
@@ -60,7 +62,7 @@ describe('hmmm', () => {
       new Field(-1)
     );
 
-    const outcome_cancel: Outcome = new Outcome(
+    outcome_cancel = new Outcome(
       CircuitString.fromString(
         'One of the parties decided to cancel the contract'
       ),
@@ -71,7 +73,7 @@ describe('hmmm', () => {
       new Field(-1)
     );
 
-    const mac_contract: Preimage = new Preimage(
+    mac_contract = new Preimage(
       CircuitString.fromString(
         'The contractor will do the job and arbiter will verify it'
       ),
@@ -83,23 +85,53 @@ describe('hmmm', () => {
       outcome_failure,
       outcome_cancel
     );
+  });
 
+  afterAll(async () => {
+    setTimeout(shutdown, 0);
+  });
+
+  it('should correctly serialize/deserialize a participant', () => {
+    const serialized: Field[] = employer.serialize();
+    const deserialized: Participant = Participant.deserialize(serialized);
+    const [d, l]: [Participant, Field[]] =
+      Participant.deserializeBuffer(serialized);
+    expect(l.length).toEqual(0);
+  });
+
+  it('should correctly serialize/deserialize an outcome', () => {
+    const serialized: Field[] = outcome_success.serialize();
+    const deserialized: Outcome = Outcome.deserialize(serialized);
+    const [d, l]: [Outcome, Field[]] = Outcome.deserializeBuffer(serialized);
+    expect(l.length).toEqual(0);
+  });
+
+  it('should correctly serialize/deserialize a whole contract', () => {
+    const [serialized, contract_string]: [Field[], string] =
+      mac_contract.serialize();
+    const deserialized: Preimage = Preimage.deserialize(
+      serialized,
+      contract_string
+    );
+  });
+
+  it('should correctly identify the participants by their keys', () => {
     Circuit.runAndCheck(() => {
-      mac_contract.isEmployer(employer_pk).assertEquals(Bool(true));
-      mac_contract.isEmployer(contractor_pk).assertEquals(Bool(false));
-      mac_contract.isEmployer(arbiter_pk).assertEquals(Bool(false));
+      mac_contract.isEmployer(employer.pk()).assertEquals(Bool(true));
+      mac_contract.isEmployer(contractor.pk()).assertEquals(Bool(false));
+      mac_contract.isEmployer(arbiter.pk()).assertEquals(Bool(false));
 
-      mac_contract.isContractor(employer_pk).assertEquals(Bool(false));
-      mac_contract.isContractor(contractor_pk).assertEquals(Bool(true));
-      mac_contract.isContractor(arbiter_pk).assertEquals(Bool(false));
+      mac_contract.isContractor(employer.pk()).assertEquals(Bool(false));
+      mac_contract.isContractor(contractor.pk()).assertEquals(Bool(true));
+      mac_contract.isContractor(arbiter.pk()).assertEquals(Bool(false));
 
-      mac_contract.isArbiter(employer_pk).assertEquals(Bool(false));
-      mac_contract.isArbiter(contractor_pk).assertEquals(Bool(false));
-      mac_contract.isArbiter(arbiter_pk).assertEquals(Bool(true));
+      mac_contract.isArbiter(employer.pk()).assertEquals(Bool(false));
+      mac_contract.isArbiter(contractor.pk()).assertEquals(Bool(false));
+      mac_contract.isArbiter(arbiter.pk()).assertEquals(Bool(true));
 
-      mac_contract.isParty(employer_pk).assertEquals(Bool(true));
-      mac_contract.isParty(contractor_pk).assertEquals(Bool(true));
-      mac_contract.isParty(arbiter_pk).assertEquals(Bool(true));
+      mac_contract.isParty(employer.pk()).assertEquals(Bool(true));
+      mac_contract.isParty(contractor.pk()).assertEquals(Bool(true));
+      mac_contract.isParty(arbiter.pk()).assertEquals(Bool(true));
     });
   });
 });
--- contracts/src/preimage.ts
+++ contracts/src/preimage.ts
@@ -17,8 +17,29 @@ export class Participant extends CircuitValue {
     this.participant_address = participant_address;
   }
 
+  pk(): PublicKey {
+    return this.participant_address;
+  }
+
   serialize(): Field[] {
     return this.toFields();
+  }
+
+  static deserialize(serialized: Field[]): Participant {
+    const deserialized: Participant = Participant.ofFields(serialized);
+    if (deserialized === null) {
+      throw Error();
+    }
+    return deserialized;
+  }
+
+  static deserializeBuffer(serialized: Field[]): [Participant, Field[]] {
+    const len: number = 2;
+    const tot: number = serialized.length;
+    const deserialized: Participant = Participant.deserialize(
+      serialized.slice(0, len)
+    );
+    return [deserialized, serialized.slice(len, tot)];
   }
 }
 
@@ -55,6 +76,21 @@ export class Outcome extends CircuitValue {
   serialize(): Field[] {
     return this.toFields();
   }
+
+  static deserialize(serialized: Field[]): Outcome {
+    const deserialized: Outcome = Outcome.ofFields(serialized);
+    if (deserialized === null) {
+      throw Error();
+    }
+    return deserialized;
+  }
+
+  static deserializeBuffer(serialized: Field[]): [Outcome, Field[]] {
+    const len: number = 133;
+    const tot: number = serialized.length;
+    const deserialized: Outcome = Outcome.deserialize(serialized.slice(0, len));
+    return [deserialized, serialized.slice(len, tot)];
+  }
 }
 
 export class Preimage extends CircuitValue {
@@ -84,6 +120,8 @@ export class Preimage extends CircuitValue {
     super();
 
     this.protocol_version = Field(0);
+
+    this.contract = contract;
 
     this.employer = employer;
     this.contractor = contractor;
@@ -136,12 +174,90 @@ export class Preimage extends CircuitValue {
   }
 
   // handling the serialization
-  serialize(): Field[] {
-    return this.toFields();
+  serialize(): [Field[], string] {
+    let serialized: Field[] = [this.protocol_version];
+    serialized = serialized.concat(this.employer.serialize());
+    serialized = serialized.concat(this.contractor.serialize());
+    serialized = serialized.concat(this.arbiter.serialize());
+    serialized = serialized.concat(this.deposited.serialize());
+    serialized = serialized.concat(this.success.serialize());
+    serialized = serialized.concat(this.failure.serialize());
+    serialized = serialized.concat(this.cancel.serialize());
+
+    const contract_string: string = this.contract.toString();
+    return [serialized, contract_string];
+  }
+
+  static deserialize(serialized: Field[], contract_string: string): Preimage {
+    const protocol_version: Field = serialized[0];
+    protocol_version.assertEquals(Field(0));
+
+    let employer: Participant;
+    let contractor: Participant;
+    let arbiter: Participant;
+
+    let outcome_deposited: Outcome;
+    let outcome_success: Outcome;
+    let outcome_failure: Outcome;
+    let outcome_cancel: Outcome;
+
+    let mac_contract: Preimage;
+    let rem: Field[] = serialized.slice(1, serialized.length);
+
+    [employer, rem] = Participant.deserializeBuffer(rem);
+    if (employer === null) {
+      throw Error();
+    }
+
+    [contractor, rem] = Participant.deserializeBuffer(rem);
+    if (contractor === null) {
+      throw Error();
+    }
+
+    [arbiter, rem] = Participant.deserializeBuffer(rem);
+    if (arbiter === null) {
+      throw Error();
+    }
+
+    [outcome_deposited, rem] = Outcome.deserializeBuffer(rem);
+    if (outcome_deposited === null) {
+      throw Error();
+    }
+
+    [outcome_success, rem] = Outcome.deserializeBuffer(rem);
+    if (outcome_success === null) {
+      throw Error();
+    }
+
+    [outcome_failure, rem] = Outcome.deserializeBuffer(rem);
+    if (outcome_failure === null) {
+      throw Error();
+    }
+
+    [outcome_cancel, rem] = Outcome.deserializeBuffer(rem);
+    if (outcome_cancel === null) {
+      throw Error();
+    }
+
+    mac_contract = new Preimage(
+      CircuitString.fromString(contract_string),
+      employer,
+      contractor,
+      arbiter,
+      outcome_deposited,
+      outcome_success,
+      outcome_failure,
+      outcome_cancel
+    );
+    return mac_contract;
   }
 
   getCommitment(): Field {
-    return Poseidon.hash(this.serialize());
+    const [serialized, contract_string]: [Field[], string] = this.serialize();
+    const params_hash: Field = Poseidon.hash(serialized);
+    const contract: CircuitString = CircuitString.fromString(contract_string);
+    const contract_hash: Field = contract.hash();
+    return Poseidon.hash([params_hash, contract_hash]);
   }
 
   // shareable contract
