dc9e04630993bab1d8a3dff65d526c02bcfc9312
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-12-08 08:23:57 +0100

    Used next/link component to avoid page reloading

--- ui/components/navbar.tsx
+++ ui/components/navbar.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link'
+
 const CircuitCompileButton = ( {state, setState} ) => {
     const isCompiled = state.isLoggedIn;
     if (state.compiled == 0) {
         return <button className="btn" onClick={() => {
-            console.log(typeof(setState));
             state.runCompile(state, setState);
         }}>
             Compile
@@ -21,36 +22,29 @@ const CircuitCompileButton = ( {state, setState} ) => {
 const Navbar = ( {state, setState} ) => {
     return (
         <ul className="menu menu-vertical lg:menu-horizontal bg-base-100 rounded-box">
-            <li><a href="/">MAC</a></li>
+            <li><Link href="/">MAC</Link></li>
             <li tabIndex={0}>
                 <span>Contract</span>
                 <ul className="rounded-box bg-base-200 p-2">
                     <li className="menu-title">
                         <span>Create</span>
                     </li>
-                    <li><a href="/create">New</a></li>
+                    <li><Link href="/create">New</Link></li>
                     <li className="menu-title">
                         <span>Interact</span>
                     </li>
-                    <li><a href="/current">Current</a></li>
-                    <li><a>Close</a></li>
+                    <li><Link href="/current">Current</Link></li>
+                    <li>Close</li>
                     <li className="menu-title">
                         <span>IO</span>
                     </li>
-                    <li><a href="/import">Import</a></li>
-                    <li><a href="/export">Export</a></li>
+                    <li><Link href="/import">Import</Link></li>
+                    <li><Link href="/export">Export</Link></li>
                 </ul>
             </li>
-            <li><a href="/about">About</a></li>
+            <li><Link href="/about">About</Link></li>
             <li>
                 <CircuitCompileButton state={state} setState={setState} />
-            </li>
-            <li>
-                <select className="select w-28">
-                    <option disabled>Mainnet</option>
-                    <option selected>Berkeley</option>
-                    <option disabled>Local</option>
-                </select>
             </li>
             <li><button className="btn">Connect</button></li>
         </ul>
