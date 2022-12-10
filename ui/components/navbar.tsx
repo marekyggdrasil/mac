e6472660081cce6fa9121001bd0a79624d0504cf
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-12-10 12:01:46 +0100

    Fixed menu under the mobile view

--- ui/components/navbar.tsx
+++ ui/components/navbar.tsx
@@ -76,22 +76,16 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <a className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <ul  tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link href="/create">New zkApp</Link></li>
+                        <li><Link href="/current">Interact</Link></li>
+                        <li><Link href="/close">Close</Link></li>
+                        <li><Link href="/import">Import zkApp</Link></li>
+                        <li><Link href="/export">Export zkApp</Link></li>
+                        <li><Link href="/about">About</Link></li>
                     </ul>
                 </div>
-                <Link className="btn btn-ghost normal-case text-xl" href="/">MAC</Link>
+                <Link className="btn btn-ghost normal-case text-xl" href="/">MAC!</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
