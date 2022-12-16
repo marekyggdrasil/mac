6e3abd2adbfc3e34b5e96170b89b15468a64e730
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-12-17 00:58:20 +0100

    Deploy gh-pages

---
+++ ui/out/_next/static/chunks/pages/index-ef349b2b33c7d1c5.js
@@ -0,0 +1,205 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[405],{
+
+/***/ 9208:
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/",
+      function () {
+        return __webpack_require__(5128);
+      }
+    ]);
+    if(false) {}
+  
+
+/***/ }),
+
+/***/ 5128:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ Home; }
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+
+
+function Home() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("article", {
+        className: "container prose",
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
+                className: "hero min-h-screen bg-base-200",
+                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
+                    className: "hero-content text-center",
+                    children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "max-w-md",
+                        children: [
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("h1", {
+                                className: "text-5xl font-bold",
+                                children: "Hello there!"
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                                className: "py-6",
+                                children: [
+                                    "I am ",
+                                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("i", {
+                                        children: "MAC!"
+                                    }),
+                                    ", or ",
+                                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("i", {
+                                        children: "Mina Arbitrated Contracts"
+                                    }),
+                                    " if you want to be formal. I am an ",
+                                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                                        href: "",
+                                        children: "zkApp"
+                                    }),
+                                    " that makes zkApps (yes, really!)."
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                href: "/create",
+                                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("button", {
+                                    className: "btn btn-primary",
+                                    children: "Get Started"
+                                })
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                                children: [
+                                    "The best part is you do not even need to know how to code! If you are confused check the ",
+                                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                                        href: "/about",
+                                        children: "about page"
+                                    }),
+                                    " to learn more!"
+                                ]
+                            })
+                        ]
+                    })
+                })
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                children: [
+                    "If you are new to blockchain and totally confused what is this website about, you might first want to check what is ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                        href: "https://minaprotocol.com/",
+                        target: "_blank",
+                        rel: "noreferrer",
+                        children: "Mina Protocol"
+                    }),
+                    " developed by ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                        href: "https://o1labs.org/",
+                        children: "O(1) Labs"
+                    }),
+                    " and what are ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                        href: "https://minaprotocol.com/zkapps",
+                        target: "_blank",
+                        rel: "noreferrer",
+                        children: "zkApps"
+                    }),
+                    ". For sure you heard of apps and maybe even of ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                        href: "https://en.wikipedia.org/wiki/Smart_contract",
+                        target: "_blank",
+                        rel: "noreferrer",
+                        children: "Smart Contracts"
+                    }),
+                    " but most likely you never though you might make one, now theres your chance!"
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("i", {
+                        children: "MAC!"
+                    }),
+                    " is one of those ideas that could bring blockchain technology to real life. It is a contract building engine that allows you to define some sort of criterion and disburse funds based on satisfaction of this criterion. The possibilities are only limited by your imagination! You you could even use it to secure real life interaction that do not even involve MINA coin transactions, all you need is set the payment to 0.00 MINA and let ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("i", {
+                        children: "MAC!"
+                    }),
+                    " deposit amounts secure your real-life interaction."
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                children: [
+                    "Once you define your Smart Contract you can use ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("i", {
+                        children: "MAC!"
+                    }),
+                    " to deploy it and export it under a form of MacPacks, which are encoded zero-knowledge circuits in an easy to share form. Another innovation of ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("i", {
+                        children: "MAC!"
+                    }),
+                    "."
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                children: [
+                    "This is an early and highly experimental implementation of ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("i", {
+                        children: "MAC!"
+                    }),
+                    ". It supports two modes, ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                        href: "https://www.aurowallet.com/",
+                        target: "_blank",
+                        rel: "noreferrer",
+                        children: "AURO wallet mode"
+                    }),
+                    " and PrivateKey mode - which requires you to provide your private key. The AURO mode is extremely buggy, wallet often breaks and refuses to provide the address after windows event indicating account change. As soon as AURO wallet gets debugged and works correctly (especially for deployment) the private key mode is going to be removed."
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                children: [
+                    "If something does not load properly you may also try one deployed to ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                        href: "https://marekyggdrasil.github.io/mac/",
+                        target: "_blank",
+                        rel: "noreferrer",
+                        children: "GitHub"
+                    }),
+                    ". Make sure you also check the ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                        href: "https://youtu.be/08VD7Rz7Yfo",
+                        target: "_blank",
+                        rel: "noreferrer",
+                        children: "YouTube demo"
+                    }),
+                    " and leave a like under my ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                        href: "https://twitter.com/MarekNarozniak/status/1603899192815935489",
+                        target: "_blank",
+                        rel: "noreferrer",
+                        children: "Tweet"
+                    }),
+                    " or ",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", {
+                        href: "https://www.facebook.com/marekstreams/posts/pfbid02xq36jZ3CnCaHL3GAKqugkD7VHbFoBbbz3s8Lg4ggCDvxB3vnw6g6kvrU5aDmX8Znl",
+                        target: "_blank",
+                        rel: "noreferrer",
+                        children: "Facebook post"
+                    }),
+                    "."
+                ]
+            })
+        ]
+    });
+}
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, [774,888,179], function() { return __webpack_exec__(9208); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
